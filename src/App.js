c1a46ecfd45f4a091e2424ebcd8e54884fefa3f8
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-07 00:48:52 -0300

    teoria useRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import ContadorUseState from "./components/ContadorUseState";
 import "./App.css";
-import "./styles/styles.css"
+import "./styles/styles.css";
 import ContadorUseReducer from "./components/ContadorUseReducer/ContadorUseReducer";
 // import ContadorCustomHook from "./components/ContadorCustomHook/ContadorCustomHook";
 
@@ -11,6 +11,9 @@ export default function App() {
       {/* <ContadorUseState></ContadorUseState> */}
       {/* <ContadorCustomHook></ContadorCustomHook> */}
       <ContadorUseReducer></ContadorUseReducer>
+      {/* Teoria de useRef */}
+      {/* Hook que se usa para almacenar valores que no queremos que se re-rendericen en pantalla cuando mutan */}
+      {/* Usado en intervals, y en manipulacion de DOM */}
     </div>
   );
 }
