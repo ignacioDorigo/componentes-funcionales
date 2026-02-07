335f70fbb3355fb03d07997d43125c3eac1da31e
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2026-02-06 22:00:43 -0300

    contador useState

---
+++ src/components/ContadorCustomHook/ContadorCustomHook.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ContadorCustomHook() {
+  return <div>ContadorCustomHook</div>;
+}
---
+++ src/components/ContadorUseState/ContadorUseState.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./ContadorUseState.css";
+
+export default function ContadorUseState() {
+  const [contador, setContador] = useState(0);
+
+  const sumar = () => {
+    setContador((prevState) => prevState + 1);
+  };
+  const restar = () => {
+    if (contador !== 0) {
+      setContador((prevState) => prevState - 1);
+    }
+  };
+
+  return (
+    <div className="contadorUseState">
+      <h1>Contador: {contador}</h1>
+
+      <div className="contador__botones">
+        <button onClick={sumar}>Sumar</button>
+        <button onClick={restar}>Restar</button>
+      </div>
+    </div>
+  );
+}
